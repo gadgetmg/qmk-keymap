e553ed97091fac2e688b946f94754387faf53122
Matt Graham <gadgetmg@pm.me>
2024-09-11 16:27:21 -0400

    fix: Fix missed modifiers in Remote Desktop Connection

--- keyboards/ymdk/id75/v2/keymaps/gadgetmg/config.h
+++ keyboards/ymdk/id75/v2/keymaps/gadgetmg/config.h
@@ -3,6 +3,9 @@
 /* Increase default debounce */
 #define DEBOUNCE 10
 
+/* Fix missed modifiers in Remote Desktop Connection */
+#define TAP_CODE_DELAY 100
+
 #define TAPPING_TERM 170
 #define PERMISSIVE_HOLD
 
